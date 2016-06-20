8f3cbba52ba0245bb0c6d7f1298633c3365fb69d
mauro89 <mauro_rc2@hotmail.com>
2016-06-20 16:06:29 -0300

    Detalles

--- listarTipos.php
+++ listarTipos.php
@@ -1,24 +1,13 @@
-<?php
-	include("comprobarSesionActiva.php");
+	<?php include("comprobarSesionActiva.php");
     $csa=new ComprobarSesionActiva();
 	$csa->ComprobarSesion();
-	include("head.php");
-	include("encabezado.php");
-?>
-	<script type="text/javascript">
-			function confirmar(url)
-			{
-				if (!confirm("Seguro que quiere eliminar el tipo de Couch?")) { return false; }
-				else { document.location = url; return true; }
-			}
-	</script>
+	include("head.php"); include("encabezado.php"); ?>
+	<script type="text/javascript"> function confirmar(url) { if (!confirm("Seguro que quiere eliminar el tipo de Couch?")) { return false; } else { document.location = url; return true; } } </script>
     <section class="div_listado">
-	<?php
-		include_once ("connection.php");
+		<?php include_once ("connection.php");
 		$con=connection();
 		$consul="SELECT * FROM tipo_hospedaje";
-		$resul=mysqli_query($con,$consul);
-    ?>
+		$resul=mysqli_query($con,$consul); ?>
 	<div class="columnat">TIPOS DE COUCH</div>
 		</br>
 		<table>
@@ -28,21 +17,15 @@
 				<td> Editar</td>
 				<td> Eliminar</td>
 			</tr>
-		<?php
-		while($fila=mysqli_fetch_array($resul))
-		{	?> 
+		<?php while($fila=mysqli_fetch_array($resul)) {	?> 
 			<tr>
 				<td><?php echo $fila['nombre_tipo']; ?></td>
 				<td><?php echo $fila['estado']; ?></td>
-				<td><a href="formulario.php?var=5&id_tipo=<?php echo $fila['id_tipo']; ?>" > Editar </a> </td>
-				<td><a href="javascript:;" onclick="confirmar('eliminarTipo.php?id_tipo=<?php echo $fila['id_tipo']; ?>'); return false;"><img src="img/eliminar.gif" width=15px height=15px></a>				</td>
+				<td style="width:100px;"><a href="formulario.php?var=5&id_tipo=<?php echo $fila['id_tipo']; ?>" > <img src="img/editar.png" style="width:100px; height:32px;"> </a> </td>
+				<td style="width:100px;"><a href="javascript:;" onclick="confirmar('eliminarTipo.php?id_tipo=<?php echo $fila['id_tipo']; ?>'); return false;"><img src="img/borrar.png" style="width:100px; height:32px;"> </a></td>
 			</tr>   
-			<?php
-		}
-		?> 
+			<?php } ?> 
 		</table>
-    </br>
-	<a href="formulario.php?var=4"><input type="button" value="Agregar un nuevo tipo"></a>
 </section>
 <?php include("footer.php") ?>
 </body>
